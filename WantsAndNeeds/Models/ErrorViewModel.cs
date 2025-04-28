8e2306d9f16742ae6ed48ec54c622e133fd851d8
thekevinhu <22kevinhu@gmail.com>
2025-04-27 21:23:42 -0400

    init

---
+++ WantsAndNeeds/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WantsAndNeeds.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
